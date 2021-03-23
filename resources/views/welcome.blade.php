7720dbfd13dd2cfdb2d4766ef49809892fff443c
katbi89 <katbi89@gmail.com>
2021-03-23 23:15:44 +0200

    Editing

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+      
+        <title>Asia Center</title>
+        <meta content="" name="descriptison">
+        <meta content="" name="keywords">
+      
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+      
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/vendor/icofont/icofont.min.css" rel="stylesheet">
+        <link href="/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="/vendor/venobox/venobox.css" rel="stylesheet">
+        <link href="/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="/vendor/aos/aos.css" rel="stylesheet">
+      
+        <!-- Template Main CSS File -->
+        <link href="/css/style.css" rel="stylesheet">
+      
+      
+      </head>
+      
+      <body>
+      
+        <!-- ======= Mobile nav toggle button ======= -->
+        <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+      
+        <!-- ======= Header ======= -->
+        <header id="header" class="d-flex flex-column justify-content-center">
+      
+          <nav class="nav-menu">
+            <ul>
+              <li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
+              <li><a href="#services"><i class="bx bx-server"></i> <span>ICDL</span></a></li>
+              <li><a href="#about"><i class="bx bx-user"></i> <span>About Us</span></a></li>
+              <li><a href="#contact"><i class="bx bx-envelope"></i> <span>Contact Us</span></a></li>
+            </ul>
+          </nav><!-- .nav-menu -->
+      
+        </header><!-- End Header -->
+      
+        <!-- ======= Hero Section ======= -->
+        <section id="hero" class="d-flex flex-column justify-content-center">
+          <div class="container" data-aos="zoom-in" data-aos-delay="100">
+            <h1>Asia Center</h1>
+            <p><span class="typed" data-typed-items="Training center, developing skills, Business Administration, Certified exam center"></span></p>
+            <div class="social-links">
+              <a href="#" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+              {{-- <a href="#" target="_blank" class="github"><i class="bx bxl-github"></i></a> --}}
+              {{-- <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a> --}}
+            </div>
+          </div>
+        </section><!-- End Hero -->
+      
+        <main id="main">
+      
+          <!-- ======= ICDL Section ======= -->
+          <section id="services" class="services">
+            <div class="container" data-aos="fade-up">
+      
+              <div class="section-title">
+                <h2>ICDL</h2>
+                <p>Reservation ICDL Exam Online</p>
+              </div>
+      
+              <div class="row">
+      
+                <div class="col-lg-12 col-md-12" data-aos="zoom-in" data-aos-delay="100">
+                  <div class="icon-box iconbox-blue">
+                    <div class="icon">
+                      <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,521.0016835830174C376.1290562159157,517.8887921683347,466.0731472004068,529.7835943286574,510.70327084640275,468.03025145048787C554.3714126377745,407.6079735673963,508.03601936045806,328.9844924480964,491.2728898941984,256.3432110539036C474.5976632858925,184.082847569629,479.9380746630129,96.60480741107993,416.23090153303,58.64404602377083C348.86323505073057,18.502131276798302,261.93793281208167,40.57373210992963,193.5410806939664,78.93577620505333C130.42746243093433,114.334589627462,98.30271207620316,179.96522072025542,76.75703585869454,249.04625023123273C51.97151888228291,328.5150500222984,13.704378332031375,421.85034740162234,66.52175969318436,486.19268352777647C119.04800174914682,550.1803526380478,217.28368757567262,524.383925680826,300,521.0016835830174"></path>
+                      </svg>
+                      <i class="bx bx-pen"></i>
+                    </div>
+                    <h4><a href="http://178.253.110.33:1200/" target="_blank">Reservation Exam</a></h4>
+                    <p>Reservation ICDL Exam Online</p>
+                  </div>
+                </div>
+      
+                {{-- <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                  <div class="icon-box iconbox-orange ">
+                    <div class="icon">
+                      <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,582.0697525312426C382.5290701553225,586.8405444964366,449.9789794690241,525.3245884688669,502.5850820975895,461.55621195738473C556.606425686781,396.0723002908107,615.8543463187945,314.28637112970534,586.6730223649479,234.56875336149918C558.9533121215079,158.8439757836574,454.9685369536778,164.00468322053177,381.49747125262974,130.76875717737553C312.15926192815925,99.40240125094834,248.97055460311594,18.661163978235184,179.8680185752513,50.54337015887873C110.5421016452524,82.52863877960104,119.82277516462835,180.83849132639028,109.12597500060166,256.43424936330496C100.08760227029461,320.3096726198365,92.17705696193138,384.0621239912766,124.79988738764834,439.7174275375508C164.83382741302287,508.01625554203684,220.96474134820875,577.5009287672846,300,582.0697525312426"></path>
+                      </svg>
+                      <i class="bx bx-microphone"></i>
+                    </div>
+                    <h4><a href="#" target="_blank">Information</a></h4>
+                    <p>About ICDL Exams</p>
+                  </div>
+                </div>
+      
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+                  <div class="icon-box iconbox-blue">
+                    <div class="icon">
+                      <svg width="100" height="100" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke="none" stroke-width="0" fill="#f5f5f5" d="M300,541.5067337569781C382.14930387511276,545.0595476570109,479.8736841581634,548.3450877840088,526.4010558755058,480.5488172755941C571.5218469581645,414.80211281144784,517.5187510058486,332.0715597781072,496.52539010469104,255.14436215662573C477.37192572678356,184.95920475031193,473.57363656557914,105.61284051026155,413.0603344069578,65.22779650032875C343.27470386102294,18.654635553484475,251.2091493199835,5.337323636656869,175.0934190732945,40.62881213300186C97.87086631185822,76.43348514350839,51.98124368387456,156.15599469081315,36.44837278890362,239.84606092416172C21.716077023791087,319.22268207091537,43.775223500013084,401.1760424656574,96.891909868211,461.97329694683043C147.22146801428983,519.5804099606455,223.5754009179313,538.201503339737,300,541.5067337569781"></path>
+                      </svg>
+                      <i class="bx bx-face"></i>
+                    </div>
+                    <h4><a href="#" target="_blank">Downloads</a></h4>
+                    <p>All About ICDL Exams</p>
+                  </div>
+                </div>
+       --}}
+      
+              </div>
+      
+            </div>
+          </section><!-- End ICDL Section -->
+          <!-- ======= About Section ======= -->
+          <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+      
+              <div class="section-title">
+                <h2>About</h2>
+                <p>Who is Asia Center</p>
+              </div>
+      
+              <div class="row">
+                <div class="col-lg-12">
+                  <img src="#" class="img-fluid" alt="">
+                </div>
+                {{-- <div class="col-lg-12 pt-4 pt-lg-0 content"> --}}
+                  {{-- <h3>Text &amp; Text</h3>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                      </ul>
+                    </div>
+                    <div class="col-lg-6">
+                      <ul>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                        <li><i class="icofont-rounded-right"></i> <strong>Text</strong>Text</li>
+                      </ul>
+                    </div>
+                  </div> --}}
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, nihil itaque, consectetur adipisci maxime obcaecati magnam cumque alias beatae corporis minima. Beatae perspiciatis, rem voluptas temporibus ipsam natus nesciunt sint!
+                  </p>
+                </div>
+              </div>
+      
+            </div>
+          </section><!-- End About Section -->
+      
+          <!-- ======= Facts Section ======= -->
+          <section id="facts" class="facts">
+            <div class="container" data-aos="fade-up">
+      
+              <div class="section-title">
+                <h2>Facts</h2>
+                <p>Let's have a look on the numbers</p>
+              </div>
+      
+              <div class="row">
+      
+                <div class="col-lg-3 col-md-6">
+                  <div class="count-box">
+                    <i class="icofont-simple-smile"></i>
+                    <span data-toggle="counter-up">588</span>
+                    <p>Students</p>
+                  </div>
+                </div>
+      
+                <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+                  <div class="count-box">
+                    <i class="icofont-document-folder"></i>
+                    <span data-toggle="counter-up">21</span>
+                    <p>Courses</p>
+                  </div>
+                </div>
+      
+                <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                  <div class="count-box">
+                    <i class="icofont-live-support"></i>
+                    <span data-toggle="counter-up">1,463</span>
+                    <p>Certificate</p>
+                  </div>
+                </div>
+      
+                <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                  <div class="count-box">
+                    <i class="icofont-users-alt-5"></i>
+                    <span data-toggle="counter-up">80</span>
+                    <p>Teammates</p>
+                  </div>
+                </div>
+      
+              </div>
+      
+            </div>
+          </section><!-- End Facts Section -->
+      
+          <!-- ======= Skills Section ======= -->
+          {{-- <section id="skills" class="skills section-bg">
+            <div class="container" data-aos="fade-up">
+      
+              <div class="section-title">
+                <h2>Our Courses Now</h2>
+                <p>Yep, Let's see what i got.</p>
+              </div>
+      
+              <div class="row skills-content">
+      
+                <div class="col-lg-6">
+      
+                  <div class="progress">
+                    <span class="skill">HTML <i class="val">80%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                  <div class="progress">
+                    <span class="skill">CSS <i class="val">75%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                  <div class="progress">
+                    <span class="skill">JavaScript <i class="val">60%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                </div>
+      
+                <div class="col-lg-6">
+      
+                  <div class="progress">
+                    <span class="skill">Python <i class="val">75%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                  <div class="progress">
+                    <span class="skill">ICDL <i class="val">55%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                  <div class="progress">
+                    <span class="skill">Photoshop <i class="val">75%</i></span>
+                    <div class="progress-bar-wrap">
+                      <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+      
+                </div>
+      
+              </div>
+      
+            </div>
+          </section><!-- End Courses Section --> --}}
+      
+      
+          <!-- ======= Contact Section ======= -->
+          <section id="contact" class="contact">
+            <div class="container" data-aos="fade-up">
+      
+              <div class="section-title">
+                <h2>Contact</h2>
+              </div>
+      
+              <div class="row mt-1">
+      
+                <div class="col-lg-12 text-center">
+                  <div class="info">
+                    <div class="address">
+                      <i class="icofont-google-map"></i>
+                      <h4>Location:</h4>
+                      <p>Damascus - Halboni</p>
+                    </div>
+      
+                    <div class="email">
+                      <i class="icofont-envelope"></i>
+                      <h4>Email:</h4>
+                      <p>info@asia-sy.com</p>
+                    </div>
+      
+                  </div>
+      
+                </div>
+      
+                {{-- <div class="col-lg-8 mt-5 mt-lg-0">
+                  <p>Text<br> Text
+                    Text<br> Text</p> --}}
+      
+                </div>
+      
+              </div>
+      
+            </div>
+          </section><!-- End Contact Section -->
+      
+        </main><!-- End #main -->
+      
+        <!-- ======= Footer ======= -->
+        <footer id="footer">
+          <div class="container text-left">
+            <h3>Asia Center</h3>
+            <div class="social-links">
+              <a href="#" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+              {{-- <a href="#" target="_blank" class="github"><i class="bx bxl-github"></i></a> --}}
+              {{-- <a href="#" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a> --}}
+            </div>
+            <div class="copyright">
+
+              <p class="love">Made with  <span class="heart"></span> Bilal Katbi</p>
+            </div>
+            <div class="credits">
+              Designed using <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+          </div>
+        </footer><!-- End Footer -->
+      
+      
+        <!-- Vendor JS Files -->
+        <script src="/vendor/jquery/jquery.min.js"></script>
+        <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/vendor/jquery.easing/jquery.easing.min.js"></script>
+        <script src="/vendor/php-email-form/validate.js"></script>
+        <script src="/vendor/waypoints/jquery.waypoints.min.js"></script>
+        <script src="/vendor/counterup/counterup.min.js"></script>
+        <script src="/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="/vendor/venobox/venobox.min.js"></script>
+        <script src="/vendor/owl.carousel/owl.carousel.min.js"></script>
+        <script src="/vendor/typed.js/typed.min.js"></script>
+        <script src="/vendor/aos/aos.js"></script>
+      
+        <!-- Template Main JS File -->
+        <script src="/js/main.js"></script>
+      
+        {{-- <script async>(function(w, d) { w.CollectId = "5f4008971773836e21485455"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script> --}}
+      
+      </body>
+
+</html>
